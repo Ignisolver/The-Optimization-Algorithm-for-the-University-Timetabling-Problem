4fffcb999fcf97c75ec6e507a5d0487d0ef20996
IgnacyNPG <kaignacy@gmail.com>
2022-10-13 12:17:59 +0200

    week schedule tested

--- data_presentation/data_presentation.py
+++ data_presentation/data_presentation.py
@@ -14,33 +14,33 @@ def generate_pdfs(groups, lecturers, rooms, name):
                    ("lecturers", lecturers),
                    ("rooms", rooms))
     for name, items in names_items:
-        generate_pdfs_for_items(result_path, name, items)
+        _generate_pdfs_for_items(result_path, name, items)
 
 
-def generate_pdfs_for_items(path: Path, name, items: List[WithSchedule]):
+def _generate_pdfs_for_items(path: Path, name, items: List[WithSchedule]):
     folder_path = path.joinpath(name)
     os.mkdir(folder_path)
     for item in items:
         name = item.name
         file_path = folder_path.joinpath(name)
         yaml_txt = item.week_schedule.to_yaml()
-        save_yaml(file_path, yaml_txt)
-        save_pdf(file_path)
-        delete_yaml(file_path)
+        _save_yaml(file_path, yaml_txt)
+        _save_pdf(file_path)
+        _delete_yaml(file_path)
 
 
-def save_yaml(path: Path, text):
+def _save_yaml(path: Path, text):
     path = path.with_suffix(".yaml")
     with open(path, 'w') as file:
         file.write(text)
 
 
-def save_pdf(path: Path):
+def _save_pdf(path: Path):
     yaml_path = path.with_suffix(".yaml")
     pdf_path = path.with_suffix(".pdf")
-    system(f'py -3.10 -m pdfschedule --no-weekends "{yaml_path}" "{pdf_path}"')
+    print(system(f'py -3.10 -m pdfschedule "{yaml_path}" "{pdf_path}"'))
 
 
-def delete_yaml(path):
+def _delete_yaml(path):
     yaml_path = path.with_suffix(".yaml")
     os.remove(yaml_path)
--- tests/data_presentation_generation/test_data_presentation.py
+++ tests/data_presentation_generation/test_data_presentation.py
@@ -1,3 +1,10 @@
+import os
+from pathlib import Path
+
+from data_presentation.data_presentation import _save_yaml, _delete_yaml, \
+    _save_pdf
+
+
 def test_generate_pdfs():
     assert False
 
@@ -6,13 +13,26 @@ def test_generate_pdfs_for_items():
     assert False
 
 
-def test_save_yaml():
-    assert False
+def test_save_delete_yaml():
+    f_name = "temp_test"
+    folder_path = os.curdir
+    file_path = Path(folder_path).joinpath(f"{f_name}.yaml")
+    if f"{f_name}.yaml" in os.listdir(folder_path):
+        os.remove(file_path)
+
+    _save_yaml(file_path, "KOZA: 1")
+    assert f"{f_name}.yaml" in os.listdir(folder_path)
+    _delete_yaml(file_path)
+    assert f"{f_name}.yaml" not in os.listdir(folder_path)
 
 
 def test_save_pdf():
-    assert False
-
-
-def test_delete_yaml():
-    assert False
+    f_name = "temp_test"
+    folder_path = os.curdir
+    file_path = Path(folder_path).joinpath(f"{f_name}.yaml")
+    yaml_txt = """- name: Work to live\n  days: MTWRF\n  time: 9-17"""
+    _save_yaml(file_path, yaml_txt)
+    _save_pdf(file_path)
+    assert f"{f_name}.pdf" in os.listdir(folder_path)
+    os.remove(file_path.with_suffix(".pdf"))
+    _delete_yaml(file_path)
